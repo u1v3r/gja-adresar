8e64c02f5f30c788f442c968b51c43e3ae672b06
jurij <hynek.jurij@gmail.com>
2012-03-07 22:19:52 +0100

    filtr v tabulce v pohledu database

--- src/abook/gui/tabs/AbTabDatabase.java
+++ src/abook/gui/tabs/AbTabDatabase.java
@@ -7,10 +7,13 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
+import abook.listeners.AbEvent;
+import abook.listeners.AbListener;
+import abook.listeners.InitListenerCore;
 import abook.profile.AbPerson;
 import abook.profile.InitProfile;
 
-public class AbTabDatabase implements AbITabComponent {
+public class AbTabDatabase implements AbITabComponent, AbListener {
 	
 	protected JScrollPane panel;
 	protected final String name = "Database";
@@ -23,6 +26,8 @@ public class AbTabDatabase implements AbITabComponent {
 		createTable();
 		
 		this.panel = new JScrollPane(table);
+		
+		InitListenerCore.getListenerCore().addListener(this);
 	}
 
 	private void createTable() {
@@ -35,10 +40,37 @@ public class AbTabDatabase implements AbITabComponent {
 		tableModel.addColumn("Location");
 		tableModel.addColumn("Groups");
 		
-		// make rows //
+		table = new JTable(tableModel);
+		
+		actualizeTable();
+		
+	}
+	
+	private void actualizeTable() {
+		
+		// clear table //
+		int rowCount = tableModel.getRowCount();
+		for(int i = 0; i < rowCount; i++) {
+			tableModel.removeRow(0);
+		}
+		
+		// add new rows //
 		List<AbPerson> listOfPersons = InitProfile.getProfile().getListOfAbPersons();
 		List<String> listOfGroups = InitProfile.getProfile().getListOfGroups();
+		List<Integer> listOfSelectedGroups = InitProfile.getProfile().getListOfSelectedGroups();
+		boolean selected;
 		for(AbPerson person : listOfPersons) {
+			
+			// group filter //
+			selected = false;
+			for(Integer group : person.getListOfGroupIndex()) {
+				if(listOfSelectedGroups.contains(group)) {
+					selected = true;
+					break;
+				}
+			}
+			
+			if(!selected) continue;
 			
 			String[] row = new String[4];
 			row[0] = person.getFirstName();
@@ -54,8 +86,7 @@ public class AbTabDatabase implements AbITabComponent {
 			tableModel.addRow(row);
 		}
 		
-		table = new JTable(tableModel);
-		
+		table.repaint();
 	}
 
 	@Override
@@ -72,5 +103,16 @@ public class AbTabDatabase implements AbITabComponent {
 	public String getTooltip() {
 		return tooltip;
 	}
+
+	@Override
+    public void myEventOccurred(AbEvent evt, int type) {
+	    
+		if(type == GROUP_SELECTION_CHANGED) {
+			
+			System.out.println("actualize");
+			
+			actualizeTable();
+		}
+    }
 
 }
